df6975f022e4223b3dbe5c5da477720baf5e22aa
Soucup Bogdan <bogdan.socup@2performant.com>
2023-10-20 13:12:05 +0300

    Add files

---
+++ index.js
@@ -0,0 +1,50 @@
+let clicks = 0;
+
+class Interpolator {
+  constructor() {
+    this.interpolatedField = 0;
+    this.previousFieldUpdate = 0;
+    this.currentUpdateMS = Date.now();
+    this.lastUpdateMS = 0;
+  }
+
+  after_UpdateInterpolationMetadata(field) {
+    this.interpolatedField = field;
+  }
+
+  before_UpdateInterpolateMetadata(field) {
+    this.previousFieldUpdate = field;
+    this.lastUpdateMS = this.currentUpdateMS;
+    this.currentUpdateMS = Date.now();
+  }
+
+  updateCounterField(self) {
+    const deltaMS = Date.now() - self.currentUpdateMS;
+    const percent = deltaMS / (self.currentUpdateMS - self.lastUpdateMS);
+    const diff = self.interpolatedField - self.previousFieldUpdate;
+
+    const valueToShow = Math.floor(self.previousFieldUpdate + diff * percent);
+    onFrame(valueToShow);
+  }
+
+  startTimer(milisecondsUpdate, onFrame) {
+    setInterval(() => this.updateCounterField(this, onFrame), milisecondsUpdate);
+  }
+}
+
+function onFrame(currentClick) {
+  document.getElementById('increaser').innerHTML = currentClick;
+}
+
+let interpolator = new Interpolator();
+interpolator.startTimer(11, onFrame);
+
+function apiClicksCalls() {
+  interpolator.before_UpdateInterpolateMetadata(clicks);
+  if(clicks < 900) {
+    clicks += 10;
+  }
+  interpolator.after_UpdateInterpolationMetadata(clicks);
+}
+
+setInterval(apiClicksCalls, 1000);
